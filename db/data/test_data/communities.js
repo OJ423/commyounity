1ebf5a4ebaf660a69967f94c5c9c4bd9f3131e46
OJ423 <osmith.dpm@gmail.com>
2024-06-25 09:48:32 +0100

    Add footer, nav, home page

--- db/data/test_data/communities.js
+++ db/data/test_data/communities.js
@@ -2,26 +2,26 @@ module.exports = [
   {
     community_name: "Bourton-on-the-Water",
     community_description: "A picturesque village known as the 'Venice of the Cotswolds', famous for its beautiful stone bridges and riverside setting.",
-    community_img: "https://example.com/bourton_on_the_water.jpg"
+    community_img: "https://eu-assets.simpleview-europe.com/cotswolds/imageresizer/?image=%2Fdmsimgs%2FBOU10032_copy_2048755173.jpg&action=ProductDetail"
   },
   {
     community_name: "Bibury",
     community_description: "Home to the iconic Arlington Row cottages, Bibury is a charming village in the Cotswolds with a rich history and stunning scenery.",
-    community_img: "https://example.com/bibury.jpg"
+    community_img: "https://nt.global.ssl.fastly.net/binaries/content/gallery/website/national/regions/gloucestershire-cotswolds/places/bibury/library/summer/arlington-row-cottages-bibury-gloucestershire-1414170.jpg"
   },
   {
     community_name: "Castleton",
     community_description: "Nestled in the Peak District, Castleton is renowned for its stunning caves, including the Blue John Cavern and Peveril Castle.",
-    community_img: "https://example.com/castleton.jpg"
+    community_img: "https://letsgopeakdistrict.co.uk/wp-content/uploads/2018/03/Castletonvj1.jpg"
   },
   {
     community_name: "Lacock",
     community_description: "Step back in time in Lacock, a picturesque village with medieval roots and well-preserved historic buildings, including Lacock Abbey.",
-    community_img: "https://example.com/lacock.jpg"
+    community_img: "https://handluggageonly.co.uk/wp-content/uploads/2016/09/IMG_1062.jpg"
   },
   {
     community_name: "Staithes",
     community_description: "A quaint fishing village on the North Yorkshire coast, Staithes is known for its charming cottages and scenic coastal walks.",
-    community_img: "https://example.com/staithes.jpg"
+    community_img: "https://becksidestaithes.com/mt-content/uploads/2023/07/staithesmain.jpg"
   }
 ];
