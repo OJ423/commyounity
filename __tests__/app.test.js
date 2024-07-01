e47b9d20e55587418ce79e33aae42cb9f2a6a78d
OJ423 <osmith.dpm@gmail.com>
2024-07-01 13:07:42 +0100

    Remove console log

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -289,7 +289,6 @@ describe('User Registration, Login, Forgot Password and Verification Tests', () 
       })
       .expect(200)
       .then(({ body }) => {
-        console.log(body)
         const user = body.user;
         const token = body.token
         expect(user.username).toBe("janedoe");
